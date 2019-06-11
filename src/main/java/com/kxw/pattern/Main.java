7e47ef2571187e229e107317b0518da17773f407
kangxiongwei <kangxiongwei@163.com>
2019-06-12 01:07:04 +0800

    初始化设计模式的工程

---
+++ src/main/java/com/kxw/pattern/Main.java
@@ -0,0 +1,12 @@
+package com.kxw.pattern;
+
+/**
+ * Create by kangxiongwei on 2019/6/12 1:03 AM
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello pattern");
+    }
+
+}
