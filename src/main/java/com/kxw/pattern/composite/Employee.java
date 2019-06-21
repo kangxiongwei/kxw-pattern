b949a1d59a94ffc326734a7f82a1f504b9b06a9c
kangxiongwei <kangxiongwei@163.com>
2019-06-21 08:32:08 +0800

    组合模式

---
+++ src/main/java/com/kxw/pattern/composite/Employee.java
@@ -0,0 +1,44 @@
+package com.kxw.pattern.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author kangxiongwei
+ * @date 2019/6/21 8:26 AM
+ */
+public class Employee {
+
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;   //领导下的员工
+
+    //构造函数
+    public Employee(String name,String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates(){
+        return subordinates;
+    }
+
+    @Override
+    public String toString(){
+        return ("Employee :[ Name : "+ name
+                +", dept : "+ dept + ", salary :"
+                + salary+" ]");
+    }
+
+}
